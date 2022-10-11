3e811a33b4d08005f466949ca2542d1c509aeffe
konepadith <konepadithspydee@gmail.com>
2022-10-11 12:56:51 +0700

    pelay comit

--- api/users/user.controller.js
+++ api/users/user.controller.js
@@ -5,6 +5,7 @@ const {genSaltSync,hashSync} = require ("bcrypt")
 module.exports = {
     createUser: (req,res)=>{
         const body = req.body
+        console.log(req.body)
         const salt = genSaltSync(10)
         body.password = hashSync(body.password, salt);
         create(body, (err, results)=>{
